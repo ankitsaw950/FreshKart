871486f29441e0222bc221f1c9b80fabd8091b50
ankitsaw <ankitsaw950@gmail.com>
2025-08-20 10:05:34 +0530

    adding the Lax to see whether is cookie is set or not

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -30,7 +30,7 @@ export const register = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'Lax' : 'strict',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -78,7 +78,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'Lax' : 'strict',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -120,7 +120,7 @@ export const logout = async (req, res) => {
     res.clearCookie('token', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'Lax' : 'strict',
     });
 
     return res.json({ success: true, message: "Logged Out" });
