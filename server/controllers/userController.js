4abb8552b44afe6a31df1650342fab41d692120a
ankitsaw <ankitsaw950@gmail.com>
2025-08-20 09:39:20 +0530

    Fixing the token issue

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -30,7 +30,7 @@ export const register = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'strict',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -78,7 +78,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'strict',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -120,7 +120,7 @@ export const logout = async (req, res) => {
     res.clearCookie('token', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'strict',
     });
 
     return res.json({ success: true, message: "Logged Out" });
